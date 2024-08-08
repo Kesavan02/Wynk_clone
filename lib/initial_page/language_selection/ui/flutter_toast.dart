12d2f6f42e89c57a736f98369f332bc6fef8a8cd
Kesavan02 <keshavsreenivas@gmail.com>
2024-08-08 23:50:55 +0530

    added new colors

--- lib/initial_page/language_selection/ui/flutter_toast.dart
+++ lib/initial_page/language_selection/ui/flutter_toast.dart
@@ -4,11 +4,13 @@ import 'package:fluttertoast/fluttertoast.dart';
 class FlutterToastMessage {
   Future<bool?> flutterToast(msg) {
     return Fluttertoast.showToast(
+        webPosition: Alignment.center,
+        webShowClose: true,
         gravity: ToastGravity.BOTTOM_LEFT,
         msg: msg,
         fontSize: 15,
-        backgroundColor: Colors.white10,
+        backgroundColor: Colors.black.withOpacity(.7),
         textColor: Colors.red,
-        toastLength: Toast.LENGTH_LONG);
+        toastLength: Toast.LENGTH_SHORT);
   }
 }
