bbff2b38966595adde1378846bdfd4645bfc2bcd
Kesavan02 <keshavsreenivas@gmail.com>
2024-08-14 10:29:33 +0530

    feat: responsiveness

--- lib/responsive/responiveness.dart
+++ lib/responsive/responiveness.dart
@@ -9,10 +9,11 @@ class Responsiveness {
   static bool isLandScape(BuildContext context) =>
       MediaQuery.of(context).orientation == Orientation.landscape;
 
-  static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 600;
+  static bool isDesktop(BuildContext context) => width(context) > 1100;
+  static bool isTablet(BuildContext context) =>
+      width(context) > 450 && width(context) <= 1100;
   static bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width < 600;
+      MediaQuery.of(context).size.width <= 450;
 }
 
 class CustomMediaQuery {
