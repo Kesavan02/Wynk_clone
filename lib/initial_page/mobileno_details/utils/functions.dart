34eede72ffae1a8d153431a6c38e64ed0bb897d8
Kesavan02 <keshavsreenivas@gmail.com>
2024-08-14 13:47:28 +0530

    modify: otpvalidation

--- lib/initial_page/mobileno_details/utils/functions.dart
+++ lib/initial_page/mobileno_details/utils/functions.dart
@@ -8,25 +8,16 @@ import '../../otp_validation/ui/otp_screen.dart';
 otpValidationDialogBox(context, controller, selectCountryCode) async {
   if (controller.phoneNo.text.length == 10) {
     await FirebaseAuth.instance.verifyPhoneNumber(
-      phoneNumber: selectCountryCode + controller.phoneNo.text,
+      phoneNumber: '${selectCountryCode + controller.phoneNo.text}',
       verificationCompleted: (PhoneAuthCredential credential) {},
       verificationFailed: (FirebaseAuthException e) {},
       codeSent: (String verificationId, int? resendToken) {
         Navigator.push(
             context,
             PageTransition(
-              child: Dialog(
-                  elevation: 5,
-                  backgroundColor: Colors.grey,
-                  insetAnimationDuration: const Duration(seconds: 1),
-                  insetPadding: const EdgeInsets.all(10),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0)),
-                  insetAnimationCurve: Curves.easeInOut,
-                  child: OTPScreen(
-                      mobileNumber:
-                          '$selectCountryCode ${controller.phoneNo.text}',
-                      verificationId: verificationId)),
+              child: OTPScreen(
+                  mobileNumber: '$selectCountryCode ${controller.phoneNo.text}',
+                  verificationId: verificationId),
               type: PageTransitionType.theme,
             ));
       },
